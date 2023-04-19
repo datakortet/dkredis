1e539b3f2e56b74d6b32d8f685c0b9f15d0478a4
thebjorn <bp@datakortet.no>
2023-04-19 13:32:11 +0200

    Update metadata.

--- setup.py
+++ setup.py
@@ -5,11 +5,9 @@
 """
 
 classifiers = """\
-Development Status :: 3 - Alpha
+Development Status :: 5 - Production/Stable
 Intended Audience :: Developers
 Programming Language :: Python
-Programming Language :: Python :: 2
-Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Topic :: Software Development :: Libraries
 """
